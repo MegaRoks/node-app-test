1cce3a7e9e116ad189cd2dbb9e9ed0025910c5cf
Maxim Kamenev <maxim.kamenev@bytepace.com>
2019-10-22 11:20:06 +0600

    added command

--- src/controllers/files/getFile.controller.js
+++ src/controllers/files/getFile.controller.js
@@ -8,12 +8,12 @@ const router = express.Router();
 router.get('/:url_code', async (req, res) => {
     try {
         const { url_code } = req.params;
-        const file = (await Files.findAll({ where: { url_code } }))[0];
-        if (!file) {
+        const fileDetails = (await Files.findAll({ where: { url_code } }))[0];
+        if (!fileDetails) {
             throw new Error('A file with the same name does not exist.');
         }
         return res.status(200).json({
-            file,
+            fileDetails,
         });
     } catch (err) {
         return res.status(422).json({
